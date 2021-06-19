4d41a7e3eec51925070f4419170d3f491f0b3409
polvnco <cpolanco000@gmail.com>
2021-06-19 13:25:54 -0400

    missing test cases and puml files and pseudo

--- src/main/java/ex43/websiteGenerator.java
+++ src/main/java/ex43/websiteGenerator.java
@@ -4,13 +4,20 @@
  */
 package ex43;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class websiteGenerator {
     public static void main(String[] args) {
         websiteGenerator run = new websiteGenerator();
-        run.makeDir();
+        try {
+            run.makeDir();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -39,40 +46,49 @@ public class websiteGenerator {
         return scanner.next().equals("y");
     }
 
-    private void makeDir() {
+    private void makeDir() throws IOException {
         var site = getSite();
         var a = getAuthor();
         var js = getJavaScript();
         var css = CSS();
 
-        String directory_Author = "./src/main/java/ex43/website";
+        String directory_Author = "./src/main/java/ex43/website/" + site;
         File authorDirectory = new File(directory_Author);
 
         if (!authorDirectory.exists()) {
-            if (authorDirectory.mkdir()) {
+            if (authorDirectory.mkdirs()) {
                 System.out.printf("Created ./website/%s%n", site);
             } else {
                 System.out.println("yikes");
             }
         }
 
+        var filePath = new File(String.format("./src/main/java/ex43/website/%s/index.html", site));
+
+        try (var writeToFile = new BufferedWriter(new FileWriter(filePath))) {
+            writeToFile.write(String.format("<!DOCTYPE html>%n<html>%n<head>%n"));
+            writeToFile.write(String.format("<title>%s</title>%n", site));
+            writeToFile.write(String.format("<meta name=\"author\" content=\"%s\">%n", a));
+        }
+
+        System.out.printf("Created ./website/%s" + "/index.html%n", site);
 
 
-        String directory_js = "./src/main/java/ex43/js";
+        String directory_js = "./src/main/java/ex43/website/" + site + "/js";
         File jsDirectory = new File(directory_js);
 
-        String directory_css = "./src/main/java/ex43/css";
+        String directory_css = "./src/main/java/ex43/website/" + site + "/css";
         File cssDirectory = new File(directory_css);
 
         if (!jsDirectory.exists() && js) {
-            if (jsDirectory.mkdir()) {
+            if (jsDirectory.mkdirs()) {
                 System.out.println("Created ./website/" + site + "/js/");
             } else {
                 System.out.println("yikes");
             }
         }
         if (!cssDirectory.exists() && css) {
-            if (cssDirectory.mkdir()) {
+            if (cssDirectory.mkdirs()) {
                 System.out.println("Created ./website/" + site + "/css/");
             } else {
                 System.out.println("yikes");
